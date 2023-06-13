fee50cf11356f08efc7b8b2cbb13151575f89fed
Guilherme Pedrazzi <gsppedra@hotmail.com>
2023-06-13 15:09:40 -0300

    First commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth:api',[
+            'except' =>[
+                'login',
+                'create',
+                'unauthorized'
+            ]
+        ]);
+    }
+
+    public function login(){
+        return 'OLA';
+    }
+
+    public function logout()
+    {
+        return "OLá";
+    }
+
+}

3acd2660e40328ca3aa0fb61757fb195617ca686
Guilherme Pedrazzi <gsppedra@hotmail.com>
2023-06-13 22:02:11 -0300

    User add and update OK, posting feed OK

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,6 +18,11 @@ class AuthController extends Controller
         ]);
     }
 
+    public function unauthorized()
+    {
+        return response()->json(['error' => 'Não autorizado.'],401);
+    }
+
     public function create(Request $request)
     {
         //*POST -> api/user [add] (name, email, password, birthdate)
@@ -72,13 +77,41 @@ class AuthController extends Controller
         return $array;
     }
 
-    public function login(){
-        return 'OLA';
+    public function login(Request $request){
+        $array = ['error' => ''];
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if($email && $password){
+            $token = auth()->attempt([
+                'email' => $email,
+                'password' => $password,
+            ]);
+            if(!$token){
+                $array['error'] = 'E-mail e/ou senha errados!';
+                return $array;
+            }
+            $array['token'] = $token;
+        }else{
+            $array['error'] = 'E-mail ou senha não digitados!';
+        }
+
+
+        return $array;
     }
 
     public function logout()
     {
-        return "OLá";
+        auth()->logout();
+        return ['error'=>''];
     }
+
+    public function refresh()
+    {
+        $token = auth()->refresh();
+        return ['error'=>'','token' => $token];
+    }
+
 
 }
--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,14 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Image;
 class FeedController extends Controller
 {
     private $loggedUser;
     public function __construct(){
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
+    }
+
+    public function create(Request $request)
+    {
+        $array = ['erro' => ''];
+        $allowedTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+        $type = $request->input('type');
+        $body = $request->input('body');
+        $photo = $request->file('photo');
+
+        if($type)
+        {
+            switch($type)
+            {
+                case 'text':
+                    if(!$body)
+                    {
+                        $array['error'] = 'Texto não enviado.';
+                        return $array;
+                    }
+                    break;
+                case 'photo':
+                    if($photo)
+                    {
+                        if(in_array($photo->getClientMimeType(),$allowedTypes))
+                        {
+                            $filename = md5(time().rand(0,9999)).'.jpg';
+                            $destPath = public_path('/media/uploads');
+                            $img = Image::make($photo->path())
+                                ->resize(800,null, function($constraint)
+                                {
+                                    $constraint->aspectRatio();
+                                })
+                                ->save($destPath.'/'.$filename);
+
+                            $body = $filename;
+
+                            $array['url'] = url('/media/uploads/'.$filename);
+                        }
+                        else
+                        {
+                            $array['error'] = 'Arquivo não suportado!';
+                            return $array;
+                        }
+                    }
+                    break;
+                default:
+                    $array['error'] = 'Tipo de postagem inexistente.';
+                    return $array;
+                break;
+            }
+            if($body)
+            {
+                $newPost = new Post();
+                $newPost->id_user = $this->loggedUser['id'];
+                $newPost->type = $type;
+                $newPost->created_at = date('Y-m-d H:i:s');
+                $newPost->body = $body;
+                $newPost->save();
+            }
+        }
+        else
+        {
+            $array['error'] = 'Dados não enviados.';
+            return $array;
+        }
+
+
+        return $array;
     }
 }
